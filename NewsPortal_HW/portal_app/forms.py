eb95a552701ce5d7722c2492d5614876186b614f
zhoomabekov <gzhumabekov@nu.edu.kz>
2023-03-04 05:45:17 +0600

    final

--- NewsPortal_HW/portal_app/forms.py
+++ NewsPortal_HW/portal_app/forms.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from .models import Post
 
 class PostForm(forms.ModelForm):
-    author = forms.TextInput()
+    author_user_username = forms.TextInput()
     title = forms.CharField(max_length=50)
     post_body = forms.CharField(min_length=20, widget=forms.Textarea(attrs={'rows': 5, 'cols': 100}))
     # добавил widget для расширения поля для ввода текста
