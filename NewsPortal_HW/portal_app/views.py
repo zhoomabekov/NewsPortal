062999703e21609e36df522869f126949268a521
zhoomabekov <gzhumabekov@nu.edu.kz>
2023-03-11 03:36:57 +0600

    Permissions added

--- NewsPortal_HW/portal_app/views.py
+++ NewsPortal_HW/portal_app/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
@@ -6,7 +6,8 @@ from .filters import PostFilter
 from .forms import PostForm
 
 
-class PostsList(ListView):
+class PostsList(PermissionRequiredMixin, ListView):
+    permission_required = ('portal_app.view_post')
     model = Post
     ordering = '-post_created'
     template_name = 'posts.html'
@@ -24,7 +25,8 @@ class PostsList(ListView):
         return context
 
 
-class PostsSearch(ListView):
+class PostsSearch(PermissionRequiredMixin, ListView):
+    permission_required = ('portal_app.view_post')
     model = Post
     ordering = '-post_created'
     template_name = 'posts_search.html'
@@ -42,13 +44,16 @@ class PostsSearch(ListView):
         return context
 
 
-class PostDetail(DetailView):
+class PostDetail(PermissionRequiredMixin, DetailView):
+    permission_required = ('portal_app.view_post')
     model = Post
     template_name = 'post.html'
     context_object_name = 'post'
 
 
-class PostCreate(CreateView):
+class PostCreate(PermissionRequiredMixin, CreateView):
+    permission_required = ('portal_app.add_post')
+
     form_class = PostForm
     model = Post
     template_name = 'post_edit.html'
@@ -62,13 +67,15 @@ class PostCreate(CreateView):
         return super().form_valid(form)
 
 
-class PostUpdate(LoginRequiredMixin, UpdateView):
+class PostUpdate(PermissionRequiredMixin, UpdateView):
+    permission_required = ('portal_app.change_post')
     form_class = PostForm
     model = Post
     template_name = 'post_edit.html'
 
 
-class PostDelete(DeleteView):
+class PostDelete(PermissionRequiredMixin, DeleteView):
+    permission_required = ('portal_app.delete_post')
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('posts_list')
